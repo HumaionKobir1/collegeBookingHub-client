506431959a5be7839a7aeb52508bc6f2b4875e8e
Humaion Kobir <humaionkobir341@gmail.com>
2023-07-23 01:34:25 +0600

    search btn functionality done

--- src/pages/Home/CollageCard/CollageCard.jsx
+++ src/pages/Home/CollageCard/CollageCard.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Cards from "../../../shared/Cards/Cards";
-import Banner from "../Banner/Banner";
-
+import './style.css'
 const CollageCard = () => {
     const [collegesData, setCollagesData] = useState([]);
 
@@ -14,16 +13,79 @@ const CollageCard = () => {
             console.log(data)
         })
     }, [])
+
+    const [query, setQuery] = useState('');
+
+    const handleSearch = event => {
+        event.preventDefault();
+        const search = event.target.search.value;
+        setQuery(search);
+    }
+
+    const filteredColleges = collegesData.filter(post => {
+    if (query === '') {
+      return true;
+    } else if (post.name.toLowerCase().includes(query.toLowerCase())) {
+      return true;
+    }
+    return false;
+  });
+
+  const getRandomColleges = () => {
+    const allColleges = [...collegesData];
+    allColleges.sort(() => Math.random() - 0.5);
+    return allColleges.slice(0, 2);
+  };
+
     return (
         <div>
-            <Banner></Banner>
+            <div className=" banner relative bg-gradient-to-r from-blue-500 to-purple-500 py-12">
+            <div
+                className="absolute top-0 left-0 right-0 bottom-0 opacity-50 bg-black"
+            />
+            <div
+                className="absolute top-0 left-0 right-0 bottom-0"
+                
+            />
+            <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 relative">
+                <div className="text-center">
+                <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+                    Welcome to our Booking Website
+                </h1>
+                <div className="mt-6">
+                    <form onSubmit={handleSearch} className="flex items-center justify-center max-w-md mx-auto">
+                    <input
+                        type="text"
+                        className="px-4 py-2 w-full border-2 border-transparent focus:outline-none focus:border-white rounded-l-md"
+                        placeholder="Search..."
+                        name="search"
+                    />
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-[9.5px] rounded-r-md"
+                    >
+                        Search
+                    </button>
+                    </form>
+                </div>
+                </div>
+            </div>
+            </div>
             <div className="cmt-10 md:w-[1400px] w-full mx-auto p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {
-                    collegesData.map(data => <Cards
-                        key={data.id}
-                        data={data}
-                    ></Cards>)
-                }
+                
+
+                {query === ''
+                    ? collegesData.slice(0, 9).map(data => (
+                        <Cards key={data.id} data={data}></Cards>
+                        ))
+                    : filteredColleges
+                        .slice(0, 1)
+                        .map(data => <Cards key={data.id} data={data}></Cards>)}
+                    {query !== '' && getRandomColleges().map(data => (
+                    <Cards key={data.id} data={data}></Cards>
+                ))}
+
+                
             </div>
         </div>
     );
