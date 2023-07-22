48c3492286d0b616c3da28d2e60793e533c366c5
Humaion Kobir <humaionkobir341@gmail.com>
2023-07-22 22:14:06 +0600

    some update

---
+++ src/pages/Home/CollageCard/CollageCard.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const CollageCard = () => {
+    const [collageCards, setCollageCards] = useState([]);
+
+    useEffect(()=> {
+        fetch
+    }, [])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CollageCard;
